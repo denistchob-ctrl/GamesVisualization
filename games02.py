293bbad4f30fa56392998905ed8a18ed166a6bbe
denistchob-ctrl <denis.tchob@gmail.com>
2026-04-27 09:49:12 -0300

    Update games02.py

--- games02.py
+++ games02.py
@@ -194,11 +194,11 @@ elif option.startswith("9"):
     ax0, ax1, ax2, ax3 = axes.flatten()
 
     colors = ['red', 'tan', 'lime']
-    ax0.hist(x, n_bins, normed=1, histtype='bar', color=colors, label=colors)
+    ax0.hist(x, n_bins, density=1, histtype='bar', color=colors, label=colors)
     ax0.legend(prop={'size': 10})
     ax0.set_title('bars with legend')
 
-    ax1.hist(x, n_bins, normed=1, histtype='bar', stacked=True)
+    ax1.hist(x, n_bins, density=1, histtype='bar', stacked=True)
     ax1.set_title('stacked bar')
 
     ax2.hist(x, n_bins, histtype='step', stacked=True, fill=False)
@@ -206,11 +206,11 @@ elif option.startswith("9"):
 
     # Make a multiple-histogram of data-sets with different length.
     x_multi = [np.random.randn(n) for n in [10000, 5000, 2000]]
-    ax3.hist(x_multi, n_bins, histtype='bar')
+    ax3.hist(x_multi, n_bins, density=1, histtype='bar')
     ax3.set_title('different sample sizes')
 
     fig.tight_layout()
-    st.plotly_chart(fig, use_container_width=True)
+    st.pyplot(fig)
 
 # Dashboard 7 - Tendências Temporais
 elif option.startswith("2"):
